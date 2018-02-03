e2627fc7dae1a305724d901f822030863464a76f
Sahil <sahil.garg2011@vit.ac.in>
2018-02-03 22:23:56 +0530

    Working Hello World

---
+++ src/com/sahil/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.sahil;
+
+public class HelloWorld {
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+        System.exit( 0 ); //success
+    }
+}
